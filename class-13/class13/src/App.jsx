e1c29c647213bc9f1de604f296a024a32a516ffc
Rupesh Kumar <rupesh.kumar.92.a@gmail.com>
2026-02-17 13:26:22 +0530

    install react

---
+++ class-13/class13/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Home from './components/Home'
+const App = () => {
+  return (
+    <div>
+      <Home name='hey'/>
+      <Home name='rupesh'/>
+      <Home name='I AM'/>
+  
+      
+      
+
+      
+
+     
+    </div>
+  )
+}
+
+export default App
